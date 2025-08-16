0494364806469470b319d31c121798cc3b16f066
amandadziurda <141340587+amandadziurda@users.noreply.github.com>
2025-08-16 10:34:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -24,11 +24,11 @@ const perguntas = [
             enunciado: "Quando você tem um tempo livre no fim de semana, o que gosta de fazer?",
             alternativas: [
                 {
-                    texto: "Sair para uma trilha ou atividade ao ar livre, como andar de bicicleta ou explorar novos lugares.",
+                    texto: "Sair para uma trilha ou atividade ao ar livre, como andar de bicicleta ou explorar novos lugares",
                     afirmacao: "Você certamente é aventureiro e ativo. Gosta de estar em movimento, explorando o mundo e buscando experiências novas. Valorizando a liberdade, natureza e desafios físicos."
                 },
                 {
-                    texto:  "Ficar em casa lendo um livro, assistindo filmes ou jogando videogame.",
+                    texto:  "Ficar em casa lendo um livro, assistindo filmes ou jogando videogame",
                     afirmacao: "Você certamente introspectivo e criativo. Prefere ambientes tranquilos e atividades que estimulam a mente. Gosta de mergulhar em histórias, ideias e experiências mais calmas, mas profundas."
                 }    
                
@@ -39,11 +39,11 @@ const perguntas = [
 
             alternativas: [
                 {
-                    texto: "Crescer profissionalmente, conquistar estabilidade financeira e alcançar uma posição de liderança.",
+                    texto: "Crescer profissionalmente, conquistar estabilidade financeira e alcançar uma posição de liderança",
                     afirmacao: "Você está focado(a) em crescimento, realizações e sucesso material. Provavelmente é alguém que valoriza metas claras, desafios e está disposto(a) a se esforçar bastante para conquistar reconhecimento e segurança no trabalho. Seu perfil combina com liderança, empreendedorismo ou planos de carreira bem definidos."
                 },
                {
-                    texto: "Encontrar equilíbrio pessoal, ter mais tempo livre e viver com mais propósito e bem-estar.",
+                    texto: "Encontrar equilíbrio pessoal, ter mais tempo livre e viver com mais propósito e bem-estar",
                     afirmacao: "Você busca qualidade de vida e valoriza experiências significativas mais do que conquistas materiais. Seu foco está em equilíbrio, saúde mental e viver de acordo com seus valores. Esse perfil se alinha com profissões ou estilos de vida mais flexíveis, criativos ou voltados ao autoconhecimento e impacto pessoal."
                 }    
                
